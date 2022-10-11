1c16096466e157f0b8b0e3dc58b0b992b2a7d389
abexoALX <abenezer.ashenafi.alx@gmail.com>
2022-10-11 12:23:59 +0300

    jas

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,28 +1,33 @@
 #include "sort.h"
 /**
-*bubble_sort - bubble sort algorithm
-*@array: pointer to array to sort
-*@size: number of elements
-*/
+ * bubble_sort - sorts an array of integers with Bubble Sort
+ * @array: param
+ * @size: param
+ * Return: void
+ */
 void bubble_sort(int *array, size_t size)
 {
-size_t i;
-short int check;
-int tmp;
+    size_t i, n, n1;
+    int swap;
 
-do {
-	check = 0;
-	for (i = 0; i < size; i++)
-	{
-		if (array[i] > array[i + 1])
-		{
-			tmp = array[i];
-			array[i] = array[i + 1];
-			array[i + 1] = tmp;
-			check = 1;
-			print_array(array, len);
-		}
-	}
-} while (check > 0);
+    if (array == NULL || size < 2)
+        return;
 
+    n = size;
+    while (n > 0)
+    {
+        n1 = 0;
+        for (i = 0; i < n - 1; i++)
+        {
+            if (array[i] > array[i + 1])
+            {
+                swap = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = swap;
+                n1 = i + 1;
+                print_array(array, size);
+            }
+        }
+        n = n1;
+    }
 }
